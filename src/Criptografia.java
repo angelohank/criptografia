510499affde5894a0bcf81abd7526c16e237a52f
littleMen21 <angelohank@alunos.utfpr.edu.br>
2021-10-11 13:53:47 -0300

    adicionando metodo de desencriptacao

--- src/Criptografia.java
+++ src/Criptografia.java
@@ -1,27 +1,49 @@
 public class Criptografia {
     private String textoCifrado;
+    private String textoDecifrado;
 
     public Criptografia(){
         this.textoCifrado = "";
+        this.textoDecifrado = "";
     }
 
-    public String encriptar(String frase, int id){
+    public String encriptar(String frase, int chave){
         for(int i = 0; i < frase.length(); i++){
-            this.textoCifrado += encrypt(frase.charAt(i), id);
+            this.textoCifrado += encrypt(frase.charAt(i), chave);
         }
         return this.textoCifrado;
     }
 
-    public char encrypt(char charOriginal, int id){
+    public char encrypt(char charOriginal, int chave){
         char charCifrado;
 
         if(charOriginal >= 97 && charOriginal <= 122) {
-            charCifrado = (char) ((charOriginal - 97 + id) % 26 + 97);
+            charCifrado = (char) ((charOriginal - 97 + chave) % 26 + 97);
         } else if (charOriginal >= 65 && charOriginal <= 90) {
-            charCifrado = (char) ((charOriginal - 65 + id) % 26 + 65);
+            charCifrado = (char) ((charOriginal - 65 + chave) % 26 + 65);
         } else {
             charCifrado = charOriginal;
         }
         return charCifrado;
+    }
+
+    public String desencriptar(String frase, int chave) {
+        for(int i = 0; i < frase.length(); i++){
+            this.textoDecifrado += decrypt(frase.charAt(i), chave);
+        }
+        return this.textoDecifrado;
+    }
+
+    public char decrypt(char charEncriptado, int chave) {
+        char charDecifrado;
+
+        if(charEncriptado >= 97 && charEncriptado <= 122) {
+            charDecifrado = (char) ((charEncriptado - 97 - chave) % 26 + 97);
+        } else if (charEncriptado >= 65 && charEncriptado <= 90) {
+            charDecifrado = (char) ((charEncriptado - 65 - chave) % 26 + 65);
+        } else {
+            charDecifrado = charEncriptado;
+        }
+        return charDecifrado;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,10 @@ public class Main {
 
         Criptografia criptografia = new Criptografia();
 
-        //System.out.println(criptografia.encriptar(frase, 1));
         String fraseEncriptada = criptografia.encriptar(frase, 4);
         JOptionPane.showMessageDialog(null, fraseEncriptada);
+
+        String fraseDesencriptada = criptografia.desencriptar(fraseEncriptada, 4);
+        System.out.println(fraseDesencriptada);
     }
 }
